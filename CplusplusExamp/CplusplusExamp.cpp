731eb01964b7991a30a796195e12c9312a4bd6df
Arsehha <159578130+Arsehha@users.noreply.github.com>
2024-09-27 02:09:34 +0300

    zadacha 1

---
+++ CplusplusExamp/CplusplusExamp.cpp
@@ -0,0 +1,7 @@
+﻿#include "Hello.h"
+#include <iostream>
+
+int main()
+{
+	sayHello();
+}
---
+++ CplusplusExamp/Hello.cpp
@@ -0,0 +1,7 @@
+#include "Hello.h"
+#include <iostream>
+
+void sayHello()
+{
+	std::cout << "Hello world";
+}
